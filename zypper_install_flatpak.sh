947c6da0ca09715a2962aacf788213f1b8472878
Sitnikov Timofey <tima.sitnikov@mail.ru>
2024-12-08 21:58:40 +0300

    Разбил установку графических приложений

---
+++ zypper_install_flatpak.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo zypper install -y flatpak flatpak-xdg-utils
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install --assumeyes flathub net.cozic.joplin_desktop
+sudo flatpak install --assumeyes flathub com.brave.Browser
+sudo flatpak install --assumeyes flathub com.bitwarden.desktop
+sudo flatpak install --assumeyes flathub it.mijorus.gearlever
+sudo flatpak install --assumeyes flathub org.telegram.desktop
+sudo flatpak install --assumeyes flathub org.wezfurlong.wezterm
--- zypper_install_gui.sh
+++ zypper_install_gui.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo zypper install -y \
-	flatpak flatpak-xdg-utils \
 	fira-code-fonts \
 	fontawesome-fonts \
 	fontawesome-fonts-web \
@@ -14,11 +13,3 @@ sudo zypper install -y \
 	rofi            \
 	xinit           \
 	xorg-x11
-
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install --assumeyes flathub net.cozic.joplin_desktop
-sudo flatpak install --assumeyes flathub com.brave.Browser
-sudo flatpak install --assumeyes flathub com.bitwarden.desktop
-sudo flatpak install --assumeyes flathub it.mijorus.gearlever
-sudo flatpak install --assumeyes flathub org.telegram.desktop
-sudo flatpak install --assumeyes flathub org.wezfurlong.wezterm
