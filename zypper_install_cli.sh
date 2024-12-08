36f4d4e4a27c1d10be6baa2ebba44e39f0ca521b
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-12-08 10:09:44 +0300

    Add files via upload

---
+++ zypper_install_cli.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo zypper install -y \
+	docker    docker-bash-completion    docker-fish-completion    docker-compose docker-rootless-extras \
+	dust      dust-bash-completion      dust-fish-completion                                            \
+	eza       eza-bash-completion       eza-fish-completion                                             \
+	fastfetch fastfetch-bash-completion fastfetch-fish-completion                                       \
+	fd        fd-bash-completion        fd-fish-completion                                              \
+	gum       gum-bash-completion       gum-fish-completion                                             \
+	helix     helix-bash-completion     helix-fish-completion     helix-runtime                         \
+	pnpm      pnpm-bash-completion      pnpm-fish-completion                                            \
+	ripgrep   ripgrep-bash-completion   ripgrep-fish-completion                                         \
+	sd        sd-bash-completion        sd-fish-completion                                              \
+	yq        yq-bash-completion        yq-fish-completion                                              \
+	zellij    zellij-bash-completion    zellij-fish-completion                                          \
+	btop                                                                                                \
+	fish                                                                                                \
+	git lazygit                                                                                         \
+	starship                                                                                            \
+	stow                                                                                                \
+	yast2-docker                                                                                        \
+	zoxide
