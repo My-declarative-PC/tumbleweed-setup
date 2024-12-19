94ddc5c7463bd38313f4d04fcfa79c00364e7ab1
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-12-19 10:55:45 +0300

    Update zypper_install_gui.sh
    Signed-off-by: ImperatorMarsa <tima.sitnikov@mail.ru>

--- zypper_install_gui.sh
+++ zypper_install_gui.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo zypper install -y \
-	fira-code-fonts \
-	fontawesome-fonts \
+sudo zypper install -y        \
+	fira-code-fonts       \
+	fontawesome-fonts     \
 	fontawesome-fonts-web \
-	feh             \
-	i3              \
-	i3blocks        \
-  i3lock          \
-  i3status        \
-	picom           \
-	rofi            \
-	xinit           \
+	feh                   \
+	i3                    \
+	i3blocks              \
+  	i3lock                \
+  	i3status              \
+	picom                 \
+	rofi                  \
+	xinit                 \
 	xorg-x11
