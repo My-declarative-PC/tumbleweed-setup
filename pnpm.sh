e5831ea6fd9b31e03fb9d49fc7053a393ac46282
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-12-19 11:59:52 +0300

    Create pnpm.sh
    Signed-off-by: ImperatorMarsa <tima.sitnikov@mail.ru>

---
+++ pnpm.sh
@@ -0,0 +1,9 @@
+pnpm i -g \
+  bash-language-server \
+  intelephense \
+  vscode-langservers-extracted \
+  typescript \
+  typescript-language-server \
+  dockerfile-language-server-nodejs \
+  @microsoft/compose-language-service \
+  helix-snippets-ls
