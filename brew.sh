54b45c834f7252fe9b06ccc60a9b21b11b784db4
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-01 14:20:26 +0300

    добавил пакеты необходимые

--- brew.sh
+++ brew.sh
@@ -5,3 +5,37 @@ sudo zypper in -y \
     gcc gcc-c++ \
     make automake autoconf \
     ruby3.3
+
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# brew requer
+brew install gcc
+# cli tools
+brew install  \
+    btop      \
+    dust      \
+    eza       \
+    fastfetch \
+    fd        \
+    fish      \
+    fzf       \
+    gum       \
+    helix     \
+    lazygit   \
+    node      \
+    pnpm      \
+    ripgrep   \
+    sd        \
+    starship  \
+    stow      \
+    typos-lsp \
+    yazi      \
+    yq        \
+    zellij    \
+    zoxide
+# docker
+brew install       \
+    colima         \
+    docker         \
+    docker-compose \
+    docker-completion
