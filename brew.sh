366950c08189bac5ee594f3ac61f7c5dead2ec2e
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-04 10:21:53 +0300

    Вынес установку brew
    в общие скрипты

--- brew.sh
+++ brew.sh
@@ -6,36 +6,4 @@ sudo zypper in -y \
     make automake autoconf \
     ruby3.3
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# brew requer
-brew install gcc
-# cli tools
-brew install  \
-    btop      \
-    dust      \
-    eza       \
-    fastfetch \
-    fd        \
-    fish      \
-    fzf       \
-    gum       \
-    helix     \
-    lazygit   \
-    node      \
-    pnpm      \
-    ripgrep   \
-    sd        \
-    starship  \
-    stow      \
-    typos-lsp \
-    yazi      \
-    yq        \
-    zellij    \
-    zoxide
-# docker
-brew install       \
-    colima         \
-    docker         \
-    docker-compose \
-    docker-completion
+bash common/brew/setup.sh
