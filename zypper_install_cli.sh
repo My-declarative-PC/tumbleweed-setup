150f5fcfd81b0482d25e9ac861bd9218ad35b586
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-01-04 11:01:14 +0300

    Поскольку все инструменты устанвливаются через brew
    В данных пакетах нет не кокого смысла

--- zypper_install_cli.sh
+++ zypper_install_cli.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
 
-sudo zypper install -y \
-	docker    docker-bash-completion    docker-fish-completion    docker-compose docker-rootless-extras \
-	dust      dust-bash-completion      dust-fish-completion                                            \
-	eza       eza-bash-completion       eza-fish-completion                                             \
-	fastfetch fastfetch-bash-completion fastfetch-fish-completion                                       \
-	fd        fd-bash-completion        fd-fish-completion                                              \
- 	fzf       fzf-bash-integration      fzf-fish-integration                                            \
-	gum       gum-bash-completion       gum-fish-completion                                             \
-	helix     helix-bash-completion     helix-fish-completion     helix-runtime                         \
-	pnpm      pnpm-bash-completion      pnpm-fish-completion                                            \
-	ripgrep   ripgrep-bash-completion   ripgrep-fish-completion                                         \
-	sd        sd-bash-completion        sd-fish-completion                                              \
-	yq        yq-bash-completion        yq-fish-completion                                              \
-	zellij    zellij-bash-completion    zellij-fish-completion                                          \
-	yazi      yazi-bash-completion      yazi-fish-completion                                            \
-	btop                                                                                                \
-	fish                                                                                                \
-	git lazygit                                                                                         \
-	starship                                                                                            \
-	stow                                                                                                \
-	yast2-docker                                                                                        \
-	zoxide
-
-# GitHub CLI
-sudo zypper addrepo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo zypper ref
-sudo zypper install gh
+sudo zypper install -y git yast2-docker
