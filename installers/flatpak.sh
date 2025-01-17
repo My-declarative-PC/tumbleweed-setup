0bea873365ce053dbc1f0238bcb984e3fe165756
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-01-17 14:33:39 +0300

    Добавил пакет интеграции

--- installers/flatpak.sh
+++ installers/flatpak.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo zypper install -y flatpak flatpak-xdg-utils
+sudo zypper install -y flatpak flatpak-xdg-utils xdg-desktop-portal-devel xdg-desktop-portal-gtk
 sudo zypper remove  -y MozillaFirefox MozillaFirefox-branding-openSUSE
 
 bash common/flatpak/setup.sh
