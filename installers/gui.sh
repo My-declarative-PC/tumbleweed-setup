c9578343d82276f958b357e95736606a9fc3109b
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-04-07 18:21:33 +0300

    добавил пакет для управленя медиа функциями

--- installers/gui.sh
+++ installers/gui.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-sudo zypper install -y  \
-	feh                   \
-	fira-code-fonts       \
-	fontawesome-fonts     \
-	fontawesome-fonts-web \
-	gammastep             \
-	i3                    \
-	i3lock                \
-	i3status-rust         \
-	kdeconnect-kde        \
-	maim                  \
-	picom                 \
-	rofi                  \
-	rofi-bluetooth        \
-	rofi-calc             \
-	rofi-network-manager  \
-	xinit                 \
-	xorg-x11
+sudo zypper install -y \
+    feh \
+    fira-code-fonts \
+    fontawesome-fonts \
+    fontawesome-fonts-web \
+    gammastep \
+    i3 \
+    i3lock \
+    i3status-rust \
+    kdeconnect-kde \
+    maim \
+    picom \
+    playerctl \
+    rofi \
+    rofi-bluetooth \
+    rofi-calc \
+    rofi-network-manager \
+    xinit \
+    xorg-x11
