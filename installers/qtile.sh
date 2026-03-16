80e7d4d5d8b8d98f1e1e87e3cc5b4d2eba6914ca
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2026-03-16 15:20:55 +0300

    добавил скрипты настройки uv

--- installers/qtile.sh
+++ installers/qtile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo wget https://raw.githubusercontent.com/qtile/qtile/refs/heads/master/resources/qtile.desktop \
-    -O /usr/share/xsessions/qtile.desktop
-
-sudo wget https://raw.githubusercontent.com/qtile/qtile/refs/heads/master/resources/99-qtile.rules \
-    -O /etc/udev/rules.d/99-qtile.rules
+bash common/uv/setup.sh
+bash common/uv/desktop.sh
+bash common/uv/udev-rule.sh
