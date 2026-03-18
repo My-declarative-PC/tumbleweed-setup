4c558fb4497da8b19abfa32ee5870b18c3c2830e
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2026-03-18 16:04:07 +0300

    Установил все необходимые зависимости
    для работы Qtile
    
    установленного через UV

--- installers/qtile.sh
+++ installers/qtile.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-bash common/uv/setup.sh
-bash common/qtile/desktop.sh
+## X11
+sudo zypper install \
+    python311-devel \
+    libffi-devel \
+    cairo-devel \
+    pango-devel \
+    xorg-x11-server \
+    xorg-x11-xinit \
+    libX11-devel \
+    libxcb-devel \
+    xcb-util-devel \
+    xcb-util-keysyms-devel \
+    xcb-util-wm-devel \
+    xcb-util-cursor-devel
+
+# ## Wayland
+# sudo zypper install \
+#     wlroots-devel \
+#     wayland-devel \
+#     wayland-protocols-devel
+
 bash common/qtile/udev-rule.sh
